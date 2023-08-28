7f38c59b2e70500b233a0883c814490b30511cea
Luis Davi <luisdavi.contato@gmail.com>
2023-08-28 20:51:36 -0300

    removed console.log

--- src/Events/guildMemberRemove/levelSys.js
+++ src/Events/guildMemberRemove/levelSys.js
@@ -4,6 +4,5 @@ module.exports = (member) => {
   const query = 'DELETE FROM tb_users_level WHERE user_id = ? AND guild_id = ?'
   db.run(query, [member.id, member.guild.id], (err) => {
     if (err) return console.error(err)
-    console.log('apagou')
   })
 }
